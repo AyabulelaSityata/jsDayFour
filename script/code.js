90a6dc1ec7ac52838bf1e459b49a93eed0af48a2
AyabulelaSityata <sityataayabulela@gmail.com>
2023-05-17 16:47:26 +0200

    day four

---
+++ script/code.js
@@ -0,0 +1,19 @@
+let myAddress1 = {
+    province: "western cape",
+    city: "cape town",
+    suburb: "khayelitsha",
+    zip: 7784,
+}
+console.log(typeof myAddress1);
+
+let myAddress2 = {
+    province: "KZN",
+    city: "durban",
+    suburb: "umlazi",
+    zip: 6565,
+}
+console.log(typeof myAddress2);
+
+let myAddress = ["myAddress1" , "myAddress2"];
+console.log(myAddress);
+
